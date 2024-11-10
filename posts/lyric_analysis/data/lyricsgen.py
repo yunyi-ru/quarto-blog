7ffce54872f9d14428e73e19d3fd295a0613b601
yunyi-ru <cosmicraven0530@gmail.com>
2024-11-10 03:15:06 -0500

    add ride analysis

--- posts/lyric_analysis/data/lyricsgen.py
+++ posts/lyric_analysis/data/lyricsgen.py
@@ -3,5 +3,5 @@
 import lyricsgenius
 
 genius = lyricsgenius.Genius("kv87PsmZusskx-DXiEzN-05K03T6LEyZ-8Q6DUweeXdIbjv7Epz-g-wfIz3ohDzl")
-album = genius.search_album("everything is alive", 'slowdive')
+album = genius.search_album("play ep", 'ride')
 album.save_lyrics()
